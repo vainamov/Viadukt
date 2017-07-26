f42d3bfdd7836d94d45bf337c21cb1773fecac3e
vainamov <vainamov@users.noreply.github.com>
2017-07-26 20:10:23 +0200

    Created First Basic Structure

---
+++ unfrosted/Core/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace unfrosted.Core
+{
+    internal class Connection
+    {
+        private readonly TcpClient client;
+        private readonly NetworkStream stream;
+        private readonly BinaryReader reader;
+        private readonly BinaryWriter writer;
+
+        public Connection(TcpClient client) {
+            this.client = client;
+            stream = client.GetStream();
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+        }
+
+        public void Write(byte[] buffer) {
+            writer.Write(buffer);
+        }
+
+        public byte[] Read(int count) {
+            return reader.ReadBytes(count);
+        }
+    }
+}
